6824d7c1a21cfb7c35f2a88dea66a1c1797e51cf
albert <albertheisy@mail.com>
2019-12-25 19:06:41 +0100

    Adding class wepons in game.py

--- game.py
+++ game.py
@@ -1,8 +1,34 @@
+class Rock:
+	def __init__(self):
+		self.name = 'Rock'
+		self.description = 'A fist-sized rock, suitable for bludgeoning.'
+		self.damage = 5
+	def __str__(self):
+		return self.name
+
+class Dagger:
+	def __init__(self):
+		self.name = 'Dagger'
+		self.description = 'A small dagger with some rust.'\
+							'Somewhat more dangerous than a rock.'
+		self.damage = 10
+	def __str__(self):
+		return self.name
+
+class RustySword:
+	def __init__(self):
+		self.name = 'Rusty Sword'
+		self.description = 'This sword is showing its age, '\
+							'but still has some fight in it.'
+		self.damage = 20
+	def __str__(self):
+		return self.name
+
 def get_player_command():
     return input('Action: ')
 
 def play():
-    inventory  = ['Dagger', 'Gold(5)','Crusty Bread', 'Bottle of wine']
+    inventory  = [Dagger(), 'Gold(5)','Crusty Bread', 'Bottle of wine']
     print("Escape from Cave Terror!")
     while True:
         action_input = get_player_command().lower()
